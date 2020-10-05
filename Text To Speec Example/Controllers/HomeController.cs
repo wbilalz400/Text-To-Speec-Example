9924bb4b4c377e0e834299a020e135b3a5132d7f
wbilalz400 <bilalzahid1997@hotmail.com>
2020-10-05 17:19:07 +0500

    Added Google

--- Text To Speec Example/Controllers/HomeController.cs
+++ Text To Speec Example/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Google.Cloud.TextToSpeech.V1;
+using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,7 +15,51 @@ namespace Text_To_Speec_Example.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult TextToSpeech()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TextToSpeech(string text)
+        {
+            TextToSpeechClient client = TextToSpeechClient.Create();
 
+            // Set the text input to be synthesized.
+            SynthesisInput input = new SynthesisInput
+            {
+                Text = text
+            };
+
+            // Build the voice request, select the language code ("en-US"),
+            // and the SSML voice gender ("neutral").
+            VoiceSelectionParams voice = new VoiceSelectionParams
+            {
+                LanguageCode = "en-US",
+                SsmlGender = SsmlVoiceGender.Female
+            };
+         
+
+            // Select the type of audio file you want returned.
+            AudioConfig config = new AudioConfig
+            {
+                AudioEncoding = AudioEncoding.Mp3
+            };
+
+            // Perform the Text-to-Speech request, passing the text input
+            // with the selected voice parameters and audio file type
+            var response = client.SynthesizeSpeech(new SynthesizeSpeechRequest
+            {
+                Input = input,
+                Voice = voice,
+                AudioConfig = config
+            });
+           
+            // Write the binary AudioContent of the response to an MP3 file.
+   
+                return Content(response.AudioContent.ToBase64());
+          
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
